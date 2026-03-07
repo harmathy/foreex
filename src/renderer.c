cbae55c70c02c6cafd636dfc086f1f8a7f1f9d82
Max Harmathy <harmathy@alumni.tum.de>
2026-03-07 11:27:44 +0100

    Make reuse compliant

--- src/renderer.c
+++ src/renderer.c
@@ -1,6 +1,7 @@
 /* renderer.c
  *
  * Copyright 2022 Max Harmathy <harmathy@alumni.tum.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
